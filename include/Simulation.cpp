39320caf16c78d68736d6ce89718a3b3d306f7a3
AyberkY <ayberkyaraneri@gmail.com>
2020-12-30 08:46:39 -0600

    Added a fancy 3d plot + dark mode

--- include/Simulation.cpp
+++ include/Simulation.cpp
@@ -10,6 +10,8 @@
 #include "quaternion.h"
 #include "Vector3.h"
 
+static double rad2deg = 180 / 3.14159265;
+
 void Simulation::run(int steps) {
 
 	std::ofstream dataFile(_filename);
@@ -19,6 +21,10 @@ void Simulation::run(int steps) {
 	Vector3 r_ddot;
 	Vector3 f_net;
 	Vector3 w_net;
+
+	Quaternion<double> q_ornt;
+
+	double roll,pitch,yaw;
 
 	_motor.ignite(_tStamp);
 
@@ -36,10 +42,25 @@ void Simulation::run(int steps) {
 		printf("\tAccel: <%f, %f, %f>", r_ddot.x, r_ddot.y, r_ddot.z);
 
 		_rocket.get_f_net(f_net);
-		printf("\tF-Net: <%f, %f, %f>\n", f_net.x, f_net.y, f_net.z);
+		printf("\tF-Net: <%f, %f, %f>", f_net.x, f_net.y, f_net.z);
 
 		_rocket.get_w_vect(w_net);
 		printf("\tW-Net: <%f, %f, %f>\n", w_net.x, w_net.y, w_net.z);
+
+
+		_rocket.get_q_ornt(q_ornt);
+
+		float s = q_ornt.Gets();
+		float x = q_ornt.Getx();
+		float y = q_ornt.Gety();
+		float z = q_ornt.Getz();
+
+		// eqns from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+		yaw  = atan2(2.0*(s*x + z*y), 1.0 - 2.0*(x*x + y*y)) * rad2deg;
+		pitch = asin(2.0*(s*y - z*x)) * rad2deg;
+		roll = atan2(2.0*(s*z + x*y), -1.0 + 2.0*(s*s + x*x)) * rad2deg;
+
+		printf("ROLL: %f\tPITCH: %f\tYAW: %f", roll, pitch, yaw);
 
 		_engine.march_step(_tStamp, _tStep);
 
@@ -47,7 +68,8 @@ void Simulation::run(int steps) {
 		dataFile << r_vect.x << "," << r_vect.y << "," << r_vect.z << ",";
 		dataFile << r_dot.x << "," << r_dot.y << "," << r_dot.z << ",";
 		dataFile << r_ddot.x << "," << r_ddot.y << "," << r_ddot.z << ",";
-		dataFile << f_net.x << "," << f_net.y << "," << f_net.z << "\n";
+		dataFile << f_net.x << "," << f_net.y << "," << f_net.z << ",";
+		dataFile << roll << "," << pitch << "," << yaw << "\n";
 
 		_tStamp += _tStep;
 	}
--- sim_data/plotter.py
+++ sim_data/plotter.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import csv
+
+plt.style.use('dark_background')
 
 timestamps = []
 
@@ -19,7 +22,11 @@ fx = []
 fy = []
 fz = []
 
-data_lists = [timestamps, rx, ry, rz, vx, vy, vz, ax, ay, az, fx, fy, fz]
+roll = []
+pitch = []
+yaw = []
+
+data_lists = [timestamps, rx, ry, rz, vx, vy, vz, ax, ay, az, fx, fy, fz, roll, pitch, yaw]
 
 with open("sim_data/data.csv") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -30,10 +37,33 @@ with open("sim_data/data.csv") as csvfile:
 
 
 plt.figure()
-
 plt.plot(timestamps, rz, label="Altitude")
 plt.plot(timestamps, vz, label="Vertical Velocity")
 plt.plot(timestamps, az, label="Vertical Acceleration")
+plt.plot(timestamps, roll, label="Roll")
+plt.plot(timestamps, pitch, label="Pitch")
+plt.plot(timestamps, yaw, label="yaw")
 plt.grid()
 plt.legend()
+# plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(121, projection='3d')
+ax.plot(rx,ry,rz,zdir='z')
+ax.set_xlabel("X [m]")
+ax.set_ylabel("Y [m]")
+ax.set_zlabel("Z [m]")
+ax = fig.add_subplot(322)
+ax.plot(timestamps, roll, 'r', label="Roll")
+ax.grid()
+ax.legend()
+ax = fig.add_subplot(324)
+ax.plot(timestamps, pitch, 'g', label="Pitch")
+ax.grid()
+ax.legend()
+ax = fig.add_subplot(326)
+ax.plot(timestamps, yaw, 'b', label="Yaw")
+ax.grid()
+ax.legend()
+
 plt.show()
