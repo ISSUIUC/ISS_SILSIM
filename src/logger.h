605501ec3002a550836af79079edeb530651e3f2
Ben Olaivar <olaivar3@illinois.edu>
2021-09-27 17:36:09 -0500

    added logger.h and logger.cpp for snesor data logging

---
+++ src/logger.h
@@ -0,0 +1,18 @@
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+#include <string>
+#include <vector>
+
+#include "Rocket.h"
+#include "Vector3.h"
+#include "Sensor.h"
+
+#include <iostream>
+#include <fstream>
+
+
+
+
+
+#endif
