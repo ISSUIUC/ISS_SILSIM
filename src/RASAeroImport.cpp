dad6a83c4f2eeb79303d42ae4e3af5e10882bf2f
AyberkY <ayberkyaraneri@gmail.com>
2022-01-30 23:13:11 -0600

    Lots of comments

--- src/RASAeroImport.cpp
+++ src/RASAeroImport.cpp
@@ -23,6 +23,12 @@
 
 // #define RASAERO_DEBUG
 
+/**
+ * @brief RASAeroImport class constructor. Parses data into lookup table
+ *
+ * @oaram file_path The path where the RASAero .csv data is found
+ *
+ */
 RASAeroImport::RASAeroImport(std::string file_path) {
     rapidcsv::Document csv(file_path);
 
@@ -70,6 +76,15 @@ RASAeroImport::RASAeroImport(std::string file_path) {
 #endif
 }
 
+/**
+ * @brief Set useful metadata values regarding mach number for future lookups
+ *
+ * Calculates and sets the following values:
+ * - mach_number_instances_: The number of unique mach number values that exist
+ *       in the lookup table
+ * - mach_number_fidelity_: The distance between two mach number instances
+ *
+ */
 void RASAeroImport::set_mach_number_params() {
     auto column = aero_table_.col(0);
     auto vec = std::vector<double>(column.begin(), column.end());
@@ -79,6 +94,17 @@ void RASAeroImport::set_mach_number_params() {
     mach_number_fidelity_ = fabs(vec[0] - vec[1]);
 }
 
+/**
+ * @brief Set useful metadata values regarding alpha for future lookups
+ *
+ * FYI alpha = angle-of-attack
+ *
+ * Calculates and sets the following values:
+ * - alpha_instances_: The number of unique alpha values that exist in the
+ * lookup table
+ * - alpha_fidelity_: The distance between two alpha instances
+ *
+ */
 void RASAeroImport::set_alpha_params() {
     auto column = aero_table_.col(1);
     auto vec = std::vector<double>(column.begin(), column.end());
@@ -88,6 +114,15 @@ void RASAeroImport::set_alpha_params() {
     alpha_fidelity_ = fabs(vec[0] - vec[1]);
 }
 
+/**
+ * @brief Set useful metadata values regarding protuberance for future lookups
+ *
+ * Calculates and sets the following values:
+ * - protuberance_instances_: The number of unique protuberance values that
+ * exist in the lookup table
+ * - protuberance_fidelity_: The distance between two protuberance instances
+ *
+ */
 void RASAeroImport::set_protuberance_params() {
     auto column = aero_table_.col(2);
     auto vec = std::vector<double>(column.begin(), column.end());
@@ -97,6 +132,42 @@ void RASAeroImport::set_protuberance_params() {
     protuberance_fidelity_ = fabs(vec[0] - vec[1]);
 }
 
+/** clang-format off
+ *
+ * @brief Performs a lookup and interpolates data using the Bilinear
+ * Interpolation method.
+ *
+ * This one's a doozy.
+ *
+ * The function first finds the closest mach number instance in the lookup table
+ * to the mach number that was requested. We chose not to interpolate among mach
+ * number instances because the lookup table is quite rich in mach number
+ * fidelity anyway, so just choosing the closest mach instance induces little
+ * error and simplifies the interpolation significantly.
+ *
+ * A two-dimensional interpolation is then performed among the alpha and
+ * protuberance instances that are immediately above and below the passed
+ * parameters. This is done using Bilinear Interpolation:
+ * https://en.wikipedia.org/wiki/Bilinear_interpolation
+ *
+ * The nomenclature used in the above Wiki page is translated to this
+ * implementation as follows:
+ * f(Q11) = f(alpha_below, prot_below) = row_a
+ * f(Q21) = f(alpha_above, prot_below) = row_b
+ * f(Q12) = f(alpha_below, prot_above) = row_c
+ * f(Q22) = f(alpha_above, prot_above) = row_d
+ * f(x,y1) = row_q
+ * f(x,y2) = row_r
+ *
+ * @param mach The rocket's current Mach number
+ * @param alpha The rocket's current angle-of-attack [degrees]
+ * @param protuberance The current amount of protuberance. [0.0 to 1.0]
+ *
+ * @return RASAeroCoefficients Struct instance containing rocket's aero
+ * coefficients at its current state
+ *
+ * clang-format on
+ */
 RASAeroCoefficients RASAeroImport::get_aero_coefficients(double mach,
                                                          double alpha,
                                                          double protuberance) {
@@ -156,12 +227,14 @@ RASAeroCoefficients RASAeroImport::get_aero_coefficients(double mach,
         (row_q * ((prot_above - protuberance) / (prot_above - prot_below))) +
         (row_r * ((protuberance - prot_below) / (prot_above - prot_below)));
 
+    // Contemplate the meaning of life
     RASAeroCoefficients result{row_z(3), row_z(4), row_z(5), row_z(6),
                                row_z(7), row_z(8), row_z(9)};
 
 #ifdef RASAERO_DEBUG
     std::cout << std::endl;
-    std::cout << "### [RASAeroImport get_aero_coefficients() debug above/below finding]:"
+    std::cout << "### [RASAeroImport get_aero_coefficients() debug above/below "
+                 "finding]:"
               << std::endl;
     std::cout << "mach_below = " << mach_below << std::endl;
     std::cout << "closest_mach = " << closest_mach << std::endl;
@@ -171,8 +244,9 @@ RASAeroCoefficients RASAeroImport::get_aero_coefficients(double mach,
     std::cout << "prot_above = " << prot_above << std::endl;
 
     std::cout << std::endl;
-    std::cout << "### [RASAeroImport get_aero_coefficients() debug index finding]:"
-              << std::endl;
+    std::cout
+        << "### [RASAeroImport get_aero_coefficients() debug index finding]:"
+        << std::endl;
     std::cout << "mach_start_index = " << mach_start_index << std::endl;
     std::cout << "row_a_offset = " << row_a_offset << std::endl;
     std::cout << "row_b_offset = " << row_b_offset << std::endl;
@@ -180,7 +254,8 @@ RASAeroCoefficients RASAeroImport::get_aero_coefficients(double mach,
     std::cout << "row_d_offset = " << row_d_offset << std::endl;
 
     std::cout << std::endl;
-    std::cout << "### [RASAeroImport get_aero_coefficients() debug result]:" << std::endl;
+    std::cout << "### [RASAeroImport get_aero_coefficients() debug result]:"
+              << std::endl;
     std::cout << row_z << std::endl;
 #endif
 
