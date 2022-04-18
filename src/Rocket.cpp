4ea505c2080ceac221810454492f8c1a1cc8b19b
AyberkY <ayberkyaraneri@gmail.com>
2022-04-18 11:30:15 -0500

    Thorughly commented coordinate/reference transform functions and gravity vector functions in Rocket class

--- src/Rocket.cpp
+++ src/Rocket.cpp
@@ -53,32 +53,36 @@ Vector3d Rocket::r2enu(Vector3d vector) {
 /*****************************************************************************/
 /*                        ENU FRAME <-> ECEF FRAME                           */
 /*****************************************************************************/
-
-/**
- * @brief Converts the rocket's ENU coordinates to ECEF
- *
- * Method takes in the position of the rocket in the East-North-Up reference
- * frame and pulls the latitude and longitude of the launchpad from the rocket
- * class.  The launchpad coordinates are converted into radians and used in the
- * transformation matrix.  The transformation matrix and the
- * Earth-Centered-Earth-Fixed (ecef) coordinates of the launchpad are used to
- * calculate the ecef position of the rocket.
- *
+/*
  * ENU is the distance East, North, and Up the rocket is from the origin in
- * meters ENU is a flat plane tangent to the earth, with an arbitrary but fixed
- * origin
+ * meters with East-North plane tangent to the earth, and an arbitrary but fixed
+ * origin. In the context of SILSIM, the ENU frame has its origin at the launch
+ * pad.
  *
- * ECEF is the distance the rocket is from the center of mass of the earth
- * The X axis points to the prime meridian at 180deg longitude, on the equator
+ * ECEF is the distance the rocket is from the center of mass of the earth:
+ * The X axis points to the prime meridian at 0deg longitude, on the equator
  * The Y axis points 90deg east and lies on the equator
  * The Z axis points north to form a right-handed coordinate system
  *
  * https://en.wikipedia.org/wiki/Geographic_coordinate_conversion
  * "From ENU to ECEF"
+ */
+
+/**
+ * @brief Converts launchpad-relative ENU coordinates to ECEF coordinates
  *
- * @param pos_enu Position of the rocket in the East-North-Up reference frame
+ * Method takes in the position of the rocket relative to the launchpad in the
+ * East-North-Up reference frame and pulls the latitude and longitude of the
+ * launchpad from the rocket class.  The launchpad coordinates are converted
+ * into radians and used in the transformation matrix.
+ *
+ * The transformation matrix and the Earth-Centered-Earth-Fixed (ecef)
+ * coordinates of the launchpad are then used to calculate the ecef position of
+ * the rocket.
+ *
+ * @param pos_enu Position of the rocket in the East-North-Up coordinate system
  * @return Vector3d Position of the rocket in the Earth-Centered-Earth-Fixed
- * reference frame
+ * coordinte system
  */
 Vector3d Rocket::position_enu2ecef(Vector3d pos_enu) {
     using std::sin, std::cos;
@@ -95,6 +99,22 @@ Vector3d Rocket::position_enu2ecef(Vector3d pos_enu) {
     return (transform * pos_enu) + get_launch_ecef();
 }
 
+/**
+ * @brief Converts ECEF coordinates to launchpad-relative ENU
+ * coordinates
+ *
+ * Method takes in the position of the rocket in the Earth-Centered-Earth-Fixed
+ * reference frame and subtracts the ECEF coordinates of the launch pad to yield
+ * a vector pointing from the launchpad to the rocket.
+ *
+ * It then applies the inverse of the rotation matrix used to go from ENU to
+ * ECEF to perform the opposite rotation.
+ *
+ * @param pos_ecef Position of the rocket in the Earth-Centered-Earth-Fixed
+ * coordinate system
+ * @return Vector3d Position of the rocket in the East-North-Up coordinate
+ * system
+ */
 Vector3d Rocket::position_ecef2enu(Vector3d pos_ecef) {
     using std::sin, std::cos;
 
@@ -110,6 +130,18 @@ Vector3d Rocket::position_ecef2enu(Vector3d pos_ecef) {
     return transform.transpose() * (pos_ecef - get_launch_ecef());
 }
 
+/**
+ * @brief Transforms a vector from the ENU frame to the ECEF reference frame
+ *
+ * Method applies a rotation matrix to transform a vector from the East-North-Up
+ * reference frame to the Earth-Centered-Earth-Fixed reference frame. The
+ * rotation matrix is constructed using the launchpad location stored in the
+ * Rocket class.
+ *
+ * @param vector A vector expressed in the East-North-Up reference frame
+ * @return Vector3d The same vector, now expressed in the
+ * Earth-Centered-Earth-Fixed reference frame
+ */
 Vector3d Rocket::enu2ecef(Vector3d vector) {
     using std::sin, std::cos;
 
@@ -125,6 +157,19 @@ Vector3d Rocket::enu2ecef(Vector3d vector) {
     return transform * vector;
 }
 
+/**
+ * @brief Transforms a vector from the ECEF frame to the ENU reference frame
+ *
+ * Method applies a rotation matrix to transform a vector from the
+ * Earth-Centered-Earth-Fixed reference frame to the East-North-Up reference
+ * frame. The rotation matrix is constructed using the launchpad location stored
+ * in the Rocket class.
+ *
+ * @param vector A vector expressed in the Earth-Centered-Earth-Fixed reference
+ * frame
+ * @return Vector3d The same vector, now expressed in the East-North-Up
+ * reference frame
+ */
 Vector3d Rocket::ecef2enu(Vector3d vector) {
     using std::sin, std::cos;
 
@@ -203,17 +248,41 @@ Vector3d Rocket::ecef2geod(Vector3d ecef) {
 /*****************************************************************************/
 /*                         GRAVITY DIRECTION VECTOR                          */
 /*****************************************************************************/
+/*
+ * The following functions return _unit_ vectors in the direction of the gravity
+ * vector acting on the rocket's center of mass.
+ *
+ * This gravity vector is easiest to achieve using the ECEF position vector of
+ * the rocket. Simply normalizing the ECEF position vector and inverting it
+ * yields the gravity direction vector.
+ */
 
+/**
+ * @brief Returns a unit gravity direction vector in the ECEF reference frame
+ *
+ * @return Vector3d Gravity direction vector in ECEF frame
+ */
 Vector3d Rocket::gravity_direction_vector_ecef() {
     Vector3d pos_ecef = enu2ecef(r_vect_);
     return -pos_ecef.normalized();
 }
 
+/**
+ * @brief Returns a unit gravity direction vector in the ENU reference frame
+ *
+ * @return Vector3d Gravity direction vector in ENU frame
+ */
 Vector3d Rocket::gravity_direction_vector_enu() {
     Vector3d grav_ecef = gravity_direction_vector_ecef();
     return ecef2enu(grav_ecef);
 }
 
+/**
+ * @brief Returns a unit gravity direction vector in the Rocket body reference
+ * frame
+ *
+ * @return Vector3d Gravity direction vector in Rocket body frame
+ */
 Vector3d Rocket::gravity_direction_vector_rocket() {
     Vector3d grav_enu = gravity_direction_vector_enu();
     return enu2r(grav_enu);
