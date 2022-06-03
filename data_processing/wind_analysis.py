0073ebff12a700ce5d4db804c9c367a4d138d108
AyberkY <ayberkyaraneri@gmail.com>
2022-06-03 18:04:21 -0500

    Simple python script to visualize wind for debugging/tuning

---
+++ data_processing/wind_analysis.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import logutils
+
+plt.style.use('dark_background')
+
+data, events = logutils.ingest_log("../logs/silsim_datalog.log")
+
+timestamps = data['Simulation']['timestamp'] 
+
+plt.figure()
+plt.plot(timestamps, data['Atmosphere']['current_wind_magnitude'], label="Magnitude")
+plt.plot(timestamps, data['Atmosphere']['current_wind_x'], label="Wind X")
+plt.plot(timestamps, data['Atmosphere']['current_wind_y'], label="Wind Y")
+plt.plot(timestamps, data['Atmosphere']['current_wind_z'], label="Wind Z")
+plt.grid()
+plt.legend()
+plt.show()
