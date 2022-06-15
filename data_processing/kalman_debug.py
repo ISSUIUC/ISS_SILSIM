40844d4f92be480b55447e397d2ecd8c2dee2fcf
AyberkY <ayberkyaraneri@gmail.com>
2022-06-14 21:09:49 -0500

    cool plottos

--- data_processing/kalman_debug.py
+++ data_processing/kalman_debug.py
@@ -32,6 +32,21 @@ q3 = data['KalmanFilter']['q3']
 
 kf_roll, kf_pitch, kf_yaw = quat_to_euler(q1, q2, q3, q0)
 
+
+# find when rocket pitches past 30 deg
+abort_idx = -1
+for idx, angle in enumerate(kf_pitch):
+    if angle > 30.0:
+        abort_idx = idx
+        break
+
+for idx, angle in enumerate(kf_yaw):
+    if angle > 30.0 and idx < abort_idx:
+        abort_idx = idx
+        break
+
+abort_timestamp = data['KalmanFilter']['timestamp'][abort_idx]
+
 plt.figure()
 #plt.plot(data['KalmanFilter']['timestamp'], 
 #         data['KalmanFilter']['Pos'], label="KF Estimated Position [m]")
@@ -45,19 +60,25 @@ plt.figure()
 plt.subplot(311)
 plt.plot(data['KalmanFilter']['timestamp'], kf_yaw, label="Estimated Yaw [deg]")
 plt.plot(data['Simulation']['timestamp'], data['Simulation']['yaw'],
-         linestyle='--', color='r', label="Yaw Truth")
+         linestyle='--', color='g', label="Yaw Truth")
+plt.axvline(15.0, linestyle='--', color='g', linewidth=0.5, label="Ignition")
+plt.axvline(abort_timestamp, linestyle='--', color='r', linewidth=0.8, label="Abort Trigger")
 plt.legend()
 
 plt.subplot(312)
 plt.plot(data['KalmanFilter']['timestamp'], kf_pitch, label="Estimated Pitch [deg]")
 plt.plot(data['Simulation']['timestamp'], data['Simulation']['pitch'],
-         linestyle='--', color='r', label="Pitch Truth")
+         linestyle='--', color='g', label="Pitch Truth")
+plt.axvline(15.0, linestyle='--', color='g', linewidth=0.5, label="Ignition")
+plt.axvline(abort_timestamp, linestyle='--', color='r', linewidth=0.8, label="Abort Trigger")
 plt.legend()
 
 plt.subplot(313)
 plt.plot(data['KalmanFilter']['timestamp'], kf_roll, label="Estimated Roll [deg]")
 plt.plot(data['Simulation']['timestamp'], data['Simulation']['roll'],
-         linestyle='--', color='r', label="Roll Truth")
+         linestyle='--', color='g', label="Roll Truth")
+plt.axvline(15.0, linestyle='--', color='g', linewidth=0.5, label="Ignition")
+plt.axvline(abort_timestamp, linestyle='--', color='r', linewidth=0.8, label="Abort Trigger")
 plt.legend()
 
 plt.show()
